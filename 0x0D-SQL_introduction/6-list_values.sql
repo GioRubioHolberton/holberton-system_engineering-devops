fac768848a8e6c4a3916ed6b240a072bc7f6d33e
GioRubioHolberton <759@holbertonschool.com>
2019-07-21 14:59:26 -0500

    screenshot

--- 0x0D-SQL_introduction/6-list_values.sql
+++
@@ -1,2 +0,0 @@
--- Write a script that lists all rows of the table first_table.
-SELECT * FROM first_table;
--- 0x0D-SQL_introduction/8-count_89.sql
+++
@@ -1,2 +0,0 @@
--- script that displays the number of records with id = 89.
-SELECT COUNT(id = 89) FROM first_table WHERE id = 89;
