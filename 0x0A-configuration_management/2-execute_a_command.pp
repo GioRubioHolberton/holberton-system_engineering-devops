1dcecddf3c9cddfc23e0c4e86502aef8fc18788f
GioRubioHolberton <759@holbertonschool.co>
2019-08-06 15:41:51 +0000

    Correction exercise 0, 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # kill a process killmenow that run on the machine
 exec {'killmenow':
-	path    => "/usr/bin:/usr/sbin:/bin",
-	command => 'pkill killmenow',
+  path    => "/usr/bin:/usr/sbin:/bin",
+  command => 'pkill killmenow',
 }
