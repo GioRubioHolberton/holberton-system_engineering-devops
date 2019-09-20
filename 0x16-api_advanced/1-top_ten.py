8fd7928aa10ac935ed648e471a0574b6dc044cf3
GioRubioHolberton <759@holbertonschool.com>
2019-09-20 04:47:55 +0000

    noting

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,7 +5,7 @@ import requests
 
 def top_ten(subreddit):
     """Get num subs"""
-    url_end = ("https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url_end = ('https://www.reddit.com/r/{}/about.json'.format(subreddit))
 
     my_headers = {'User-Agent': 'Gon'}
     requ = requests.get(url_end, headers=my_headers)
