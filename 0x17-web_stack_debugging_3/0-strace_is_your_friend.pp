a75247c4446591cf3949d116eaf0d0f0e4d2db86
GioRubioHolberton <759@holbertonschool.com>
2019-10-02 21:36:00 +0000

    Using strace

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # find the issue 500 error, fix it and then automate it using Puppet.
 exec { 'Fix':
-	path    => '/bin/',
-	command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+path    => '/bin/',
+command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
 }
