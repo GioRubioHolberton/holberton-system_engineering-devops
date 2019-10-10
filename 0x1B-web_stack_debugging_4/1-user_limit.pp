b80a92709a8bc2a89bcf3a89ad1ad2824abba7f5
GioRubioHolberton <759@holbertonschool.com>
2019-10-10 23:51:15 +0000

    User limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,18 +1,17 @@
 # User limit
-exec { 'replace_hard':
-command => "sed -i 's/holberton hard nofile 5/holberton hard nofile 10048/g' \
-/etc/security/limits.conf",
+exec { 'change hard':
+command  => 'sed -i "s/holberton hard nofile 5/holberton hard nofile 10048/g" \
+/etc/security/limits.conf',
 provider => shell
-}
+}->
 
-exec {'replace_soft':
-path    => 'usr/sbin',
-command => "sed -i 's/holberton soft nofile 4/holberton soft nofile 30048/g' \
-/etc/security/limits.conf",
+exec {'change soft':
+command => 'sed -i "s/holberton soft nofile 4/holberton soft nofile 30048/g" \
+/etc/security/limits.conf',
 provider => shell
-}
+}->
 
-exec { 'restart-op':
-command => 'sysctl -p',
+exec { 'restart':
+command  => 'sysctl -p',
 provider => shell
 }
